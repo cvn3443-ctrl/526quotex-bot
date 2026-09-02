04c92c9634b2ea0cc25fe196b646b1c66cba3a64
cvn3443-ctrl <cvn3443@gmail.com>
2026-09-03 01:59:11 +0300

    Update MainActivity.java

--- app/src/main/java/com/quotexbot/app/MainActivity.java
+++ app/src/main/java/com/quotexbot/app/MainActivity.java
@@ -5,18 +5,10 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.ArrayAdapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
-    private Button btnStart, btnStop;
-    private EditText editAmount;
-    private Spinner spinnerAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.webView);
-        btnStart = findViewById(R.id.btn_start);
-        btnStop = findViewById(R.id.btn_stop);
-        editAmount = findViewById(R.id.edit_amount);
-        spinnerAccount = findViewById(R.id.spinner_account);
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.account_types, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerAccount.setAdapter(adapter);
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -40,27 +23,7 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(new WebChromeClient());
 
+        // تحميل صفحة الويب المحلية
         webView.loadUrl("file:///android_asset/index.html");
-
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String amount = editAmount.getText().toString();
-                String account = spinnerAccount.getSelectedItem().toString();
-                webView.evaluateJavascript("javascript:setSettings('" + account + "', " + amount + ")", null);
-                webView.evaluateJavascript("javascript:startBot()", null);
-                btnStart.setEnabled(false);
-                btnStop.setEnabled(true);
-            }
-        });
-
-        btnStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                webView.evaluateJavascript("javascript:stopBot()", null);
-                btnStart.setEnabled(true);
-                btnStop.setEnabled(false);
-            }
-        });
     }
 }
